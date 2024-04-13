70cd1c65acb6d33db67b55bed0317d2cd4b6ee7d
extf8 <extfield8@gmail.com>
2024-04-13 16:33:46 +0300

    Updated starfield animation to fit size of view port

--- js/starfield.js
+++ js/starfield.js
@@ -1,114 +1,154 @@
-let outerspace = document.querySelector("#outerspace");
-let mainContext = outerspace.getContext('2d');
+(function () {
+    const canvas = document.getElementById('outerspace');
+    const mainContext = canvas.getContext('2d');
 
-let canvasWidth = outerspace.width;
-let canvasHeight = outerspace.height;
+    // resize the canvas to fill browser window dynamically
+    window.addEventListener('resize', resizeCanvas, false);
 
-let centerX = canvasWidth * 0.5;
-let centerY = canvasHeight * 0.5;
+    function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
 
-let numberOfStars = 1000;
-
-let stars = [];
-
-let frames_per_second = 120;
-
-let interval = Math.floor(1000 / frames_per_second);
-let startTime = performance.now();
-let previousTime = startTime;
-
-let currentTime = 0;
-let deltaTime = 0;
-
-class Star {
-  constructor() {
-    this.x = getRandomInt(-centerX, centerX);
-    this.y = getRandomInt(-centerY, centerY);
-    this.counter = getRandomInt(1, canvasWidth);
-
-    this.radiusMax = 1 + Math.random() * 2;
-    this.speed = getRandomInt(1, 1);
-
-    this.context = mainContext;
-  }
-
-  drawStar() {
-    this.counter -= this.speed;
-
-    if (this.counter < 1) {
-      this.counter = canvasWidth;
-      this.x = getRandomInt(-centerX, centerX);
-      this.y = getRandomInt(-centerY, centerY);
-
-      this.radiusMax = getRandomInt(1, 10);
-      this.speed = getRandomInt(1, 3);
+        /**
+         * Your drawings need to be inside this function otherwise they will be reset when
+         * you resize the browser window and the canvas goes will be cleared.
+         */
+        drawOuterSpace();
     }
 
-    let xRatio = this.x / this.counter;
-    let yRatio = this.y / this.counter;
+    resizeCanvas();
 
-    let starX = remap(xRatio, 0, 1, 0, canvasWidth);
-    let starY = remap(yRatio, 0, 1, 0, canvasHeight);
+    function drawOuterSpace() {
+        // do your drawing stuff here
+        let canvasWidth = outerspace.width;
+        let canvasHeight = outerspace.height;
 
-    this.radius = remap(this.counter, 0, canvasWidth, this.radiusMax, 0);
+        let centerX = canvasWidth * 0.5;
+        let centerY = canvasHeight * 0.5;
 
-    mainContext.beginPath();
+        let numberOfStars = 1000;
 
-    mainContext.arc(starX, starY, this.radius, 0, Math.PI * 2, false);
-    mainContext.closePath();
+        let stars = [];
 
-    mainContext.fillStyle = "#FFF";
-    mainContext.fill();
-  }
-}
+        let frames_per_second = 60;
 
-function setup() {
-  for (let i = 0; i < numberOfStars; i++) {
-    let star = new Star();
-    stars.push(star);
-  }
-}
-setup();
+        let interval = Math.floor(1000 / frames_per_second);
+        let startTime = performance.now();
+        let previousTime = startTime;
 
-function draw(timestamp) {
-  currentTime = timestamp;
-  deltaTime = currentTime - previousTime;
+        let currentTime = 0;
+        let deltaTime = 0;
 
-  if (deltaTime > interval) {
-    previousTime = currentTime - (deltaTime % interval);
+        class Star {
+            constructor() {
+                this.x = getRandomInt(-centerX, centerX);
+                this.y = getRandomInt(-centerY, centerY);
+                this.counter = getRandomInt(1, canvasWidth);
 
-    mainContext.clearRect(0, 0, canvasWidth, canvasHeight);
-    // mainContext.fillStyle = "#111";
-    mainContext.fillStyle = 'transparent';
+                this.radiusMax = 1 + Math.random() * 2;
+                this.speed = getRandomInt(1, 1);
 
-    mainContext.fillRect(0, 0, canvasWidth, canvasHeight);
+                this.context = mainContext;
+            }
 
-    mainContext.translate(centerX, centerY);
+            drawStar() {
+                this.counter -= this.speed;
 
-    for (let i = 0; i < stars.length; i++) {
-      let star = stars[i];
-      star.drawStar();
+                if (this.counter < 1) {
+                    this.counter = canvasWidth;
+                    this.x = getRandomInt(-centerX, centerX);
+                    this.y = getRandomInt(-centerY, centerY);
+
+                    this.radiusMax = getRandomInt(1, 3);
+                    this.speed = getRandomInt(1, 1);
+                }
+
+                let xRatio = this.x / this.counter;
+                let yRatio = this.y / this.counter;
+
+                let starX = remap(xRatio, 0, 1, 0, canvasWidth);
+                let starY = remap(yRatio, 0, 1, 0, canvasHeight);
+
+                this.radius = remap(
+                    this.counter,
+                    0,
+                    canvasWidth,
+                    this.radiusMax,
+                    0
+                );
+
+                mainContext.beginPath();
+
+                mainContext.arc(
+                    starX,
+                    starY,
+                    this.radius,
+                    0,
+                    Math.PI * 2,
+                    false
+                );
+                mainContext.closePath();
+
+                mainContext.fillStyle = '#FFF';
+                mainContext.fill();
+            }
+        }
+
+        function setup() {
+            for (let i = 0; i < numberOfStars; i++) {
+                let star = new Star();
+                stars.push(star);
+            }
+        }
+        setup();
+
+        function draw(timestamp) {
+            currentTime = timestamp;
+            deltaTime = currentTime - previousTime;
+
+            if (deltaTime > interval) {
+                previousTime = currentTime - (deltaTime % interval);
+
+                mainContext.clearRect(0, 0, canvasWidth, canvasHeight);
+                // mainContext.fillStyle = "#111";
+                mainContext.fillStyle = 'transparent';
+
+                mainContext.fillRect(0, 0, canvasWidth, canvasHeight);
+
+                mainContext.translate(centerX, centerY);
+
+                for (let i = 0; i < stars.length; i++) {
+                    let star = stars[i];
+                    star.drawStar();
+                }
+
+                mainContext.translate(-centerX, -centerY);
+            }
+
+            requestAnimationFrame(draw);
+        }
+        draw();
+
+        function getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+
+        function remap(value, istart, istop, ostart, ostop) {
+            // Ensure values are numerical to avoid potential errors
+            value = Number(value);
+            istart = Number(istart);
+            istop = Number(istop);
+            ostart = Number(ostart);
+            ostop = Number(ostop);
+
+            // Perform the mapping calculation
+            return (
+                ostart +
+                (ostop - ostart) * ((value - istart) / (istop - istart))
+            );
+        }
     }
+})();
 
-    mainContext.translate(-centerX, -centerY);
-  }
-
-  requestAnimationFrame(draw);
-}
-draw();
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function remap(value, istart, istop, ostart, ostop) {
-  // Ensure values are numerical to avoid potential errors
-  value = Number(value);
-  istart = Number(istart);
-  istop = Number(istop);
-  ostart = Number(ostart);
-  ostop = Number(ostop);
-
-  // Perform the mapping calculation
-  return ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
-}
+// let outerspace = document.querySelector('#outerspace');
+// let mainContext = outerspace.getContext('2d');
